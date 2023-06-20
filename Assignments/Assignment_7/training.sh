8b968f37d3bd03af98df6065fef91054b20603ff
Jan Claar <Jan@Uni>
2023-06-20 17:42:27 +0000

    train all

--- Assignments/Assignment_7/training.sh
+++ Assignments/Assignment_7/training.sh
@@ -4,5 +4,5 @@ python main.py --solarize --horizontal_flip;
 python main.py --solarize --crop;
 python main.py --solarize --crop --horizontal_flip;
 python main.py --solarize --crop --horizontal_flip --gauss_blur;
-#python main.py --gauss_blur --horizontal_flip;
-#python main.py --gauss_blur --crop;
+python main.py --gauss_blur --horizontal_flip;
+python main.py --gauss_blur --crop;
